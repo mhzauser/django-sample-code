05768652befe7ae0ff051f06f856f81471d3ea81
mhz <mhzuser96@gmail.com>
2019-02-08 21:38:14 +0330

    compelete test and start docs

--- trainingaurora/web/urls.py
+++ trainingaurora/web/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url , include
 from django.contrib import admin 
 from .views import Cityviewset ,Tourviewset
 from rest_framework.routers import DefaultRouter
-
+from rest_framework.documentation import include_docs_urls
 
 #router.register(r'destination' , views.Destinationviewset)
 
@@ -14,4 +14,5 @@ router.register(r'city' , Cityviewset)
 
 urlpatterns = [
     url(r'^' , include(router.urls)),
+    url(r'^docs/' , include_docs_urls(title='tour API')),
 ]
