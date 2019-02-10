906e146d0e1286fb109329de2fb1015851d4ff61
mhz <mhzuser96@gmail.com>
2019-02-10 21:42:42 +0330

    change web.urls

--- trainingaurora/web/urls.py
+++ trainingaurora/web/urls.py
@@ -3,7 +3,6 @@ from django.conf.urls import url , include
 from django.contrib import admin 
 from .views import Cityviewset ,Tourviewset
 from rest_framework.routers import DefaultRouter
-from rest_framework_docs.views import DRFDocsView
 from django.urls import reverse
 #router.register(r'destination' , views.Destinationviewset)
 
@@ -14,5 +13,5 @@ router.register(r'city' , Cityviewset)
 
 urlpatterns = [
     url(r'^' , include(router.urls)),
-    url(r'^docs/', include('rest_framework_docs.urls')),
+    
 ]
